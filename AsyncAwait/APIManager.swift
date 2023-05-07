8fa4a04c73a64ce3b37f1cfd60c7561a3b5a9dfc
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2023-05-07 11:43:53 +0900

    APIの処理をasync awaitでやってみた

--- AsyncAwait/APIManager.swift
+++ AsyncAwait/APIManager.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+class APIManager {
+
+    class func callApi<T: Codable>(url: URL?, type: T.Type) async -> T? {
+        guard let url = url else { return nil}
+
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let response = try JSONDecoder().decode(type.self, from: data)
+            return response
+        }
+        catch let error {
+            print("失敗内容:\(error)")
+            return nil
+        }
+    }
+}
