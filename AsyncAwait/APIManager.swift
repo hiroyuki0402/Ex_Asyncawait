8e43db191965dbcffd43733dea07be2309109730
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2023-05-07 11:42:44 +0900

    first commit

---
+++ AsyncAwait/APIManager.swift
@@ -0,0 +1,8 @@
+//
+//  APIManager.swift
+//  AsyncAwait
+//
+//  Created by SHIRAISHI HIROYUKI on 2023/05/07.
+//
+
+import Foundation
---
+++ AsyncAwait/UserData.swift
@@ -0,0 +1,8 @@
+//
+//  UserData.swift
+//  AsyncAwait
+//
+//  Created by SHIRAISHI HIROYUKI on 2023/05/07.
+//
+
+import Foundation
